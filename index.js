017ecb6f56e9f36991e0f958f27e96ee7cf5c895
Travis Webb <me@traviswebb.com>
2014-08-23 05:28:05 -0400

    simple bijection is working with test

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 var _ = require('lodash');
 _.mixin(require('congruence'));
 
-exports.map = function (object) {
+/**
+ * Map an input Object `x` to an output Object `y` via a bijective function `f`
+ * i.e. `f: X -> Y`
+ *
+ * @private
+ */
+function image (object, mapping) {
+  return _.mapValues(_.invert(mapping), function (y, x) {
+    return object[y];
+  });
+}
 
+/**
+ * Create a new bijection between two domains
+ */
+function Bijection (bxn) {
+  if (!_.congruent(Bijection.template, bxn)) {
+    throw new TypeError('Bijection is not valid');
+  }
+
+  this.valid = true;
+  this.bxn = bxn;
+}
+
+Bijection.template = {
+  domain: _.isObject,
+  codomain: _.isArray,
+  mapping: _.isObject
 };
+
+/**
+ * Map input object to each of the codomain subsets specified in the
+ * bijection template.
+ *
+ * @public
+ */
+Bijection.prototype.map = function map (object) {
+  return _.mapValues(this.bxn.mapping, function (mapping, name) {
+    return image(object, mapping);
+  });
+};
+
+module.exports = Bijection;
--- test.js
+++ test.js
@@ -1,14 +1,80 @@
-var _ = require('lodash');
+global._ = require('lodash');
 _.mixin(require('congruence'));
+var assert = require('assert');
 var Bijection = require('./');
+var domain = {
+  Account: require('xtuple-api/api/models/Account'),
+  Contact: require('xtuple-api/api/models/Contact'),
+  Address: require('xtuple-api/api/models/Address')
+};
+var codomain = {
+  crmacct: {
+    identity: 'crmacct',
+    attributes: {
+      crmacct_id: 'integer',
+      crmacct_number: 'string',
+      crmacct_name: 'string',
+      crmacct_active: 'boolean',
+      crmacct_type: 'string'
+    }
+  }
+};
+var xm = {
+  bijections: { }
+};
 
 describe('bijection', function () {
 
-  describe('#constructor', function () {
+  before(function () {
+    xm.bijections.Account = new Bijection({
+      domain: domain.Account,
+      codomain: [ codomain.crmacct ],
+      mapping: {
+        crmacct: {
+          id: 'crmacct_id',
+          number: 'crmacct_number',
+          name: 'crmacct_name',
+          active: 'crmacct_active',
+          type: 'crmacct_type'
+        }
+      }
+    });
+  });
 
+  describe('#constructor', function () {
+    it('should exist', function () {
+      assert(_.isFunction(Bijection));
+    });
+    it('should validate example bijection', function () {
+      assert(xm.bijections.Account.valid);
+    });
+    it('should store bijection object as .bxn property', function () {
+      assert(_.isObject(xm.bijections.Account.bxn));
+    });
+    it('should expose .map() function', function () {
+      assert(_.isFunction(xm.bijections.Account.map));
+    });
   });
 
   describe('#map', function () {
+
+    it('should return an image of the domain Y', function () {
+      var image = xm.bijections.Account.map({
+        id: 1,
+        number: '1000',
+        name: 'tjwebb',
+        active: true,
+        type: 'I'
+      });
+      assert(_.isObject(image));
+      assert(_.congruent({
+        crmacct_id: 1,
+        crmacct_number: '1000',
+        crmacct_name: 'tjwebb',
+        crmacct_active: true,
+        crmacct_type: 'I'
+      }));
+    });
 
   });
 
